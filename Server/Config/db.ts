8a0a7180a4eea3963e43952b0dea1381e5b2fbed
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-06-17 19:47:23 -0700

    final

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,3 +1,4 @@
 export const LocalURI = "mongodb://localhost/contacting_store";
-export const HostName = "localhost";
+export const RemoteURI = "mongodb+srv://bond:007@cluster-007.7syd5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
